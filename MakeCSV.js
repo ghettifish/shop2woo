d0a96d4f8ad3ceba4e22e9cf21d6380258db7596
Nicolas Bovee <admin@ghettifish.com>
2018-10-09 00:37:00 -0600

    Success! All of the pipes are completed.

--- MakeCSV.js
+++ MakeCSV.js
@@ -1,3 +1,6 @@
+const imageDir = 'http://pprs.vm/wp-content/uploads/store/';
+const showInvalid = true;
+
 const fs = require('fs');
 const Json2csvParser = require('json2csv').Parser;
 const fields = [
@@ -15,7 +18,7 @@ const fields = [
   "Position",
   "Attribute 1 name",
   "Attribute 1 value(s)",
-  "Attribute 1 visible",
+  "Attribute 1 visible"
 ];
 
 
@@ -26,7 +29,7 @@ function SortProducts() {
   this['Visibility in catalog'] = 'visible'; //visible or hidden
   this['Description'] = 'ProductDescription';
   this['In stock?'] = 1; // 1 or 0
-  this['Images'] = 'http://pprs.vm/wp-content/uploads/store/3LED-B-Lrg.jpg';
+  this['Images'] = '';
   this['Parent'] =  '';
   this['Position'] = ''; //Parent gets 0, everything else counts uses array index;
   this['Attribute 1 value(s)'] = ''; //must be 1 for the parent
@@ -37,13 +40,138 @@ function SortProducts() {
   this['Published'] = 1; // 1 or 0
   this['Allow customer reviews?'] = 0;
   this['Attribute 1 name'] = 'options';
+
+  this["buildProduct"] = function(product, type) {
+    let visibility = product['Use'] == 'checked' ? 'visible' : 'hidden';
+    let inStock = product['Use'] == 'checked' ? 1 : 0;
+    this['Type'] = type;
+    this['SKU'] = product['SKU'];
+    this['Name'] = product['Name'];
+    this['Visibility in catalog'] = visibility;
+    this['In stock?'] = inStock; // 1 or 0
+    if(product["Image"] != "none"){
+      this['Images'] = imageDir + product["Graphic"];
+    }
+    this['Position'] = 0; //Parent gets 0, everything else counts uses array index;
+    this['Attribute 1 visible'] = 1; //parent will be 1 for visible unless otherwise marked;
+    
+    this['Published'] = 1; // 1 or 0
+    this['Allow customer reviews?'] = 0;
+    this['Attribute 1 name'] = 'options';
+  }
   
 }
 
-let data = new SortProducts();
+function valid(product, logger) {
+  //Validate products
+  const valid = ["Name", "SKU", "Price"];
 
-const json2csvParser = new Json2csvParser({ fields });
-const csv = json2csvParser.parse(data);
+  if(valid.every(function(x) {
+    return x in product;
+  })) {
+    return true;
+  } else {
+    logger.push(product);
+    return false;
+  }
 
-fs.writeFile(__dirname + '/data/testing-MakeCSV.csv', csv, () => {});
+
+}
+
+
+
+function buildVariation(product, parentSKU, position, newJSON, parentPrice, parentDescription) {
+  let revProduct = new SortProducts();
+  
+  let priceModifier = parseInt(product["PriceModifier"], 10);
+  let price = product["PriceModifier"] != "" ? priceModifier : 0;
+  revProduct.buildProduct(product, 'variation');
+
+  revProduct['Parent'] = parentSKU;
+  revProduct['Position'] = position;
+  revProduct['Regular price']  =  price + parseInt(parentPrice, 10);
+  revProduct['Description'] = "";
+
+
+  newJSON.push(revProduct);
+  return;
+}
+
+function buildVariable(product, newJSON) {
+  let revProduct = new SortProducts();
+  revProduct.buildProduct(product,"variable");
+  revProduct['Attribute 1 value(s)'] = product["OptionMenus"].join(",");
+  revProduct['Regular price']  =  product["Price"];
+  revProduct['Description'] = product['ProductDescription'];
+
+  product["ProductOptions"].forEach((k,position) => {
+    buildVariation(k, product['SKU'], (position + 1), newJSON, product["Price"], product['ProductDescription']);
+  });
+
+  newJSON.push(revProduct);
+  return;
+}
+
+function buildSimple(product, newJSON) {
+  let revProduct = new SortProducts();
+  revProduct.buildProduct(product,'simple');
+  revProduct['Regular price']  =  product["Price"];
+
+  newJSON.push(revProduct);
+  return;
+}
+
+function buildJSON(filename) {
+  let file = fs.readFileSync(__dirname + filename);
+  let json = JSON.parse(file);
+  let products = json["Products"];
+
+  let newJSON = [];
+  let invalidProducts = [];
+  products.forEach(parent => {
+    
+    //Parent Level Products
+    if (valid(parent, invalidProducts)){
+      //Check for variable Products
+      if(parent.hasOwnProperty("ProductOptions")) {
+        if(parent["ProductOptions"].length >= 1) {
+          buildVariable(parent, newJSON);
+        } else {
+          buildSimple(parent, newJSON);
+        }
+      } else {
+        buildSimple(parent, newJSON);
+      }
+
+      Object.keys(parent).forEach(property => {
+      });
+    }
+  });
+
+  
+  if(invalidProducts.length > 0) {
+    console.log("There were problems with some of the products.");
+    if(showInvalid === true) {
+      invalidProducts.forEach(k => {
+        console.log(k);
+      })
+    }
+    
+  } 
+  return newJSON;
+}
+
+
+let data = buildJSON('/data/pprsfull-massaged.json');
+
+function buildCSV(data, fields){
+  const json2csvParser = new Json2csvParser({ fields });
+  const csv = json2csvParser.parse(data);
+  return csv;
+}
+
+//fs.writeFile(__dirname + '/data/testing-buildJSON.json', data, () => {});
+
+fs.writeFile(__dirname + '/data/testing-MakeCSV.csv', buildCSV(data,fields), () => {});
+
 
